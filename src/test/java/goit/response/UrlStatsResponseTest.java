49f5c779a8da2f0c8987f187d13d720079a3d2d6
Viktor Kudryavtsev <viktork8888@gmail.com>
2024-06-10 13:41:24 +0300

    fix tests

--- src/test/java/goit/response/UrlStatsResponseTest.java
+++ src/test/java/goit/response/UrlStatsResponseTest.java
@@ -8,12 +8,14 @@ class UrlStatsResponseTest {
     @Test
     void testAllArgsConstructor() {
         String shortId = "abc123";
+        String shortUrl = "http://shortener.com/";
         String longUrl = "http://example.com/long-url";
         int clickCount = 10;
 
-        UrlStatsResponse urlStatsResponse = new UrlStatsResponse(shortId, longUrl, clickCount);
+        UrlStatsResponse urlStatsResponse = new UrlStatsResponse(shortId, shortUrl, longUrl, clickCount);
 
         Assertions.assertEquals(shortId, urlStatsResponse.getShortId());
+        Assertions.assertEquals(shortUrl, urlStatsResponse.getShortUrl());
         Assertions.assertEquals(longUrl, urlStatsResponse.getLongUrl());
         Assertions.assertEquals(clickCount, urlStatsResponse.getClickCount());
     }
@@ -24,11 +26,15 @@ class UrlStatsResponseTest {
 
         String shortId = "abc123";
         urlStatsResponse.setShortId(shortId);
+        String shortUrl = "http://shortener.com/";
+        urlStatsResponse.setShortUrl(shortUrl);
         String longUrl = "http://example.com/long-url";
         urlStatsResponse.setLongUrl(longUrl);
         int clickCount = 10;
         urlStatsResponse.setClickCount(clickCount);
 
+        Assertions.assertEquals(shortId, urlStatsResponse.getShortId());
+        Assertions.assertEquals(shortUrl, urlStatsResponse.getShortUrl());
         Assertions.assertEquals(shortId, urlStatsResponse.getShortId());
         Assertions.assertEquals(longUrl, urlStatsResponse.getLongUrl());
         Assertions.assertEquals(clickCount, urlStatsResponse.getClickCount());
