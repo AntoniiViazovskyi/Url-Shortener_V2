489c704b63076b65a13dbc10881df527b77fb3a9
Belimbb <belovmladshui@gmail.com>
2024-06-11 16:21:33 +0200

    Hot fix migration files Changed tables names

--- src/main/resources/migration/h2/V1__create_db_h2.sql
+++ src/main/resources/migration/h2/V1__create_db_h2.sql
@@ -1,14 +1,14 @@
 -- Creating the Role table --
-CREATE TABLE role (
+CREATE TABLE roles (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL
 );
 
--- Creating the Role table --
-CREATE TABLE "user" (
+-- Creating the User table --
+CREATE TABLE "users" (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(100) NOT NULL CHECK (LENGTH(password) BETWEEN 8 AND 100)
 );
 
 -- Creating a table for linking users and roles (user_roles) --
@@ -16,18 +16,18 @@ CREATE TABLE user_roles (
     user_id BIGINT NOT NULL,
     role_id BIGINT NOT NULL,
     PRIMARY KEY (user_id, role_id),
-    FOREIGN KEY (user_id) REFERENCES "user" (id) ON DELETE CASCADE,
-    FOREIGN KEY (role_id) REFERENCES role (id) ON DELETE CASCADE
+    FOREIGN KEY (user_id) REFERENCES "users" (id) ON DELETE CASCADE,
+    FOREIGN KEY (role_id) REFERENCES roles (id) ON DELETE CASCADE
 );
 
 -- Creating the URL table --
-CREATE TABLE url (
+CREATE TABLE urls (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    short_id VARCHAR(255) NOT NULL,
-    long_url VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL,
+    short_id VARCHAR(20) NOT NULL UNIQUE,
+    long_url VARCHAR(500) NOT NULL,
+    creation_date TIMESTAMP NOT NULL,
+    expiry_date TIMESTAMP NOT NULL,
     click_count INT NOT NULL,
-    user_id BIGINT,
-    FOREIGN KEY (user_id) REFERENCES "user" (id) ON DELETE SET NULL
+    user_id BIGINT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES "users" (id) ON DELETE SET NULL
 );
